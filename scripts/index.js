a06aa626b1268b209f818435d59a47b89036a1e0
Nihal <nihalmujawar28413@gmail.com>
2024-06-19 18:38:27 +0530

    fetched  register button through DOM;

--- scripts/index.js
+++ scripts/index.js
@@ -3,5 +3,10 @@ let lastName = document.querySelector("#lastname");
 let email = document.querySelector("#email");
 let mobile = document.querySelector("#mobile");
 
+let button = document.querySelector("#register-button");
 let table = document.querySelector(".table");
 
+
+button.addEventListener("click",()=>{
+    
+})
