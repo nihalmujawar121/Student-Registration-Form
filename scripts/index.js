8399221377b191864fa0f264d62090318e331507
Nihal <nihalmujawar28413@gmail.com>
2024-06-19 18:33:59 +0530

    fetched the input elements and table through DOM

---
+++ scripts/index.js
@@ -0,0 +1,7 @@
+let firstName = document.querySelector("#firstname");
+let lastName = document.querySelector("#lastname");
+let email = document.querySelector("#email");
+let mobile = document.querySelector("#mobile");
+
+let table = document.querySelector(".table");
+
