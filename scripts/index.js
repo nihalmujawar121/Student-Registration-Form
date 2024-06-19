a6ff78b8fc791a56241913de8935a3b85e5151c6
Nihal <nihalmujawar28413@gmail.com>
2024-06-19 19:09:40 +0530

    created studentArray; added displayStudentDetails() function

--- scripts/index.js
+++ scripts/index.js
@@ -6,4 +6,11 @@ let mobile = document.querySelector("#mobile");
 let button = document.querySelector("#register-button");
 let tableBody = document.querySelector("#tbody");
 
+var studentArray = [];
+
 button.addEventListener("click", () => {});
+
+
+function displayStudentDetails(){
+    
+}
